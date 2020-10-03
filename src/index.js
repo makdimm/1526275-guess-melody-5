0d0d894521ecb6f766ec5d8e8cee2347a25bc8aa
MATRESHKI <makdim1993@gmail.com>
2020-10-03 15:53:38 +0300

    Компоненты, пропсы

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/app/app"
+
+const Settings = {
+  ERRORS_COUNT:33
+};
+
+ReactDOM.render(
+  <App
+    errorsCount={Settings.ERRORS_COUNT}
+    />,
+  document.querySelector('#root')
+);
+
